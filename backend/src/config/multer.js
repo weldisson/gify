f35a9a11f440d4e241fc99a53172e673421ce510
Weldisson Araujo <weldisson.a.silva@accenture.com>
2020-04-14 04:19:03 -0300

    excluindo no storage

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -21,7 +21,7 @@ const storageTypes = {
   }),
   s3: multerS3({
     s3: new aws.S3(),
-    bucket: 'upload-gify',
+    bucket: process.env.AWS_BUCKET_NAME,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: 'public-read',
     key: (req, file, cb) => {
@@ -37,7 +37,7 @@ const storageTypes = {
 };
 module.exports = {
   dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-  storage: storageTypes.local,
+  storage: storageTypes[process.env.STORAGE_TYPE],
   limits: {
     fileSize: 25 * 1024 * 1024,
   },
